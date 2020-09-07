ad9e0d02f975250254a2cb9d6efc269df45afd99
Tim Thompson <scubatim84@gmail.com>
2020-09-07 16:45:05 -0600

    Fix shadows in CategoryGridTile, add tab navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
 import { Platform } from 'react-native';
+
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import Favorites from '../screens/FavoritesScreen';
 import Colors from '../config/colors';
+import FavoritesScreen from '../screens/FavoritesScreen';
 
 const mealsNavigator = createStackNavigator(
   {
@@ -24,4 +30,38 @@ const mealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(mealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: mealsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons
+              name='ios-restaurant'
+              size={25}
+              color={tabInfo.tintColor}
+            />
+          );
+        },
+      },
+    },
+    Favorites: {
+      screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
+          );
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  }
+);
+
+export default createAppContainer(MealsFavTabNavigator);
